ec3f14041e474bb904ba97647d44f19bc977510e
Snehal652 <snehalprdc2003@gmail.com>
2024-06-22 23:23:47 +0530

    app layout

--- app/layout.js
+++ app/layout.js
@@ -21,3 +21,51 @@ export default function RootLayout({ children }) {
     </ClerkProvider>
   );
 }
+
+
+//extraa
+export default function RootLayout({ children }) {
+  return (
+    <ClerkProvider>
+    <html lang="en">
+      <body className={inter.className}>
+      <Toaster />
+      {children}</body>
+    </html>
+    </ClerkProvider>
+  );
+}
+export default function RootLayout({ children }) {
+  return (
+    <ClerkProvider>
+    <html lang="en">
+      <body className={inter.className}>
+      <Toaster />
+      {children}</body>
+    </html>
+    </ClerkProvider>
+  );
+}
+export default function RootLayout({ children }) {
+  return (
+    <ClerkProvider>
+    <html lang="en">
+      <body className={inter.className}>
+      <Toaster />
+      {children}</body>
+    </html>
+    </ClerkProvider>
+  );
+}
+export default function RootLayout({ children }) {
+  return (
+    <ClerkProvider>
+    <html lang="en">
+      <body className={inter.className}>
+      <Toaster />
+      {children}</body>
+    </html>
+    </ClerkProvider>
+  );
+}
+
